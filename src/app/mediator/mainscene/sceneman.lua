4989feabd98954d518e17d89d6a8c95bb5428db3
bin.sun <bin.sun@kaiqigu.com>
2018-05-28 20:21:33 +0800

    test current image is or not contains current point.

--- src/app/mediator/mainscene/sceneman.lua
+++ src/app/mediator/mainscene/sceneman.lua
@@ -8,9 +8,14 @@ function sceneman:ctor(image,pos)
     self:addChild(self.man);
     local function onTouch( eventType, x, y )
         if eventType == "began" then
-            local act = cc.TintBy:create(2, -150, -150, -150);
-            self.man:runAction(cc.RepeatForever:create(cc.Sequence:create({act,act:reverse()})))
-            return true;    --intercept event
+            local pt = self:convertToNodeSpace(cc, p(x, y));
+            if self.man:getBoundingBox().containsPoint(pt) then
+                local act = cc.TintBy:create(2, -150, -150, -150);
+                self.man:runAction(cc.RepeatForever:create(cc.Sequence:create({act,act:reverse()})))
+                return true;
+            else
+                return false;     
+            end
         elseif(eventType == "ended")then
             self.man:setColor(cc.c3b(255,255,255));
             self.man:stopAllActions();
